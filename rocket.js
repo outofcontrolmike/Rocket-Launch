e4cb4d9e66874cbb1ac98c80eb0011fb7a488ed8
Michael Wilson <mw0415436@otc.edu>
2018-11-17 17:26:44 -0600

    Still need to work on this

--- rocket.js
+++ rocket.js
@@ -1,6 +1,4 @@
-// opens up an HML request
 var httpRequest = new XMLHttpRequest();  // creates a variable that makes a  HMLHttpRequest  it will request data from where it's sent
-
 
 var rocketReport;   // Used in the aFunction - will eventually be turned into response data as a string
 var x;              //Variable x clears the timer 
@@ -22,6 +20,7 @@ var arianeRequest = document.getElementById("ariane");              // grabs ari
 var defaultRequest = document.getElementById("nextFive");           // grabs the nextFive button - This will display the oringinal launches from load
 var launchRows = document.getElementById("launchRows");
 
+
 //Events that are supposed to insert the API info into a certain div
 window.addEventListener("load",loadPage)                        // This event listener calls the loadPage function on load
 falconRequest.addEventListener("click", loadFalcon);             // When Falcon button is clicked - loadFalcon is run
@@ -29,7 +28,7 @@ launcheroneRequest.addEventListener("click", loadLauncherOne);  // when Launcher
 arianeRequest.addEventListener("click", loadAriane);             // when ariane is clicked - loadAriane is ran
 defaultRequest.addEventListener("click", loadLaunches);          // This calls loadLaunches function
 
-
+var httpRequest = new XMLHttpRequest();  // creates a variable that makes a  HMLHttpRequest  it will request data from where it's sent
 
 //function that loads next five launches from API to launches Div on Load Screen 
 function loadPage()
@@ -75,28 +74,18 @@ httpRequest.onreadystatechange = aFunction;
 //function converts string into an object and accesses the first launch and name of it. 
 function aFunction()
 {
-    if(httpRequest.readyState === 4)  // this only runs if the readyStae of the request client is Done === 4.  :)
+    if(httpRequest.readyState === 4 && httpRequest.status === 200)  // this only runs if the readyStae of the request client is Done === 4.  :)
         {
-            var rocketReport = httpRequest.responseText; // stores response text in a variable
+            rocketReport = httpRequest.responseText; // stores response text in a variable
             console.log(rocketReport);                  // this logs the variable that holds response text
-            var launchObject = JSON.parse(rocketReport);    // converts this string of data that is returned to an object that we can later access
+             launchObject = JSON.parse(rocketReport);    // converts this string of data that is returned to an object that we can later access
 
             console.log(launchObject.launches[0].name);     // accesses the array first value from the array and it's name value.
+
         }
         rowGet(launchObject);    // rowGet takes the info from launchObject
         timer(launchObject);    // timer takes the info from launchObject
-}
-
-// function that prints to the Html Rows by a for loop.  
-function rowGet(launchObject)    
-{
-    var row = document.getElementById("launchRows");        // grabs the table element
-    for(let i = 0; i < row.length; i++)             // starts at first value of table 
-{
-    myRows[i].innerHTML = launchObject.launches[i].name + " || " + launchObject.launches[i].net;  // places information into myRows array.        
-}
-    launchRows = myRows;        // this sets launchrows to the array myRows.  Ultimatley gives launchRows an array to compare to
-}
+        
 
 // Creating a timer - w3 Schools
 function timer(launchObject)        // Timer function that will run when launchObject is passed
@@ -104,10 +93,10 @@ function timer(launchObject)        // Timer function that will run when launchO
 clearInterval(x);       // Method that clears our timer incase there are no more launches
 
 // Set the date we're counting down to
-var countDownDate = new Date(launchObject.launches[0].net).getTime();
+countDownDate = new Date(launchObject.launches[0].net).getTime();
 
 // Update the count down every 1 second
-x = setInterval(function() {
+var x = setInterval(function() {
 
   // Get todays date and time
   var now = new Date().getTime();
@@ -133,7 +122,23 @@ x = setInterval(function() {
 }, 1000);
 }
 
- 
+}
+
+// function that prints to the Html Rows by a for loop.  
+function rowGet(launchObject)    
+{
+    var row = document.getElementById("launchRows");        // grabs the table element
+    for(let i = 0; i < row.length; i++)             // starts at first value of table 
+{
+    myRows[i].innerHTML = launchObject.launches[i].name + " || " + launchObject.launches[i].net;  // places information into myRows array.        
+}
+
+    launchRows = myRows;   // this sets launchrows to the array myRows.  Ultimatley gives launchRows an array to compare to
+}
+
+
+
+
 
 
 
